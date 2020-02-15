c4dc9d88bceef9a7fc72dd4872e8af32db1063d7
Horodrigo <hodrigocastelo@gmail.com>
2020-02-15 07:54:58 -0300

    Update insertionSort.py

--- InsertionSort/insertionSort.py
+++ InsertionSort/insertionSort.py
@@ -19,18 +19,20 @@ def plotaImagem(x,y,xl = "Entradas", yl = "Saídas", name='graph.png'):
     plt.xlabel(xl)
     fig.savefig(name)
 
-
-def selectionsort(to_sort):
-    clone = to_sort.copy()
-    length = len(clone)
-    for count, _ in enumerate(clone):
-        pivot = count
-        for count2 in range(count+1, length-1):
-            if clone[count2] < clone[pivot]:
-                pivot = count2
-        if clone[count] > clone[pivot]:
-            clone[count], clone[pivot] = clone[pivot], clone[count]
-    return clone
+def insertionSort(elementList): 
+  
+    swappedNumber = 0
+    for i in range(1, len(elementList)): 
+  
+        key = elementList[i] 
+        j = i-1
+        while j >= 0 and key < elementList[j] :
+            swappedNumber = swappedNumber + 1    
+            elementList[j + 1] = elementList[j] 
+            j -= 1
+        elementList[j + 1] = key 
+    
+    return swappedNumber
 
 
 def shuffle_random(n):
@@ -43,10 +45,9 @@ options = [1000, 10000, 30000, 60000]
 results_random = []
 results_reverse = []
 for option in options:
-    tempo =   timeit.timeit("selectionsort({})".format(shuffle_random(option)), setup="from __main__ import selectionsort",number=1)
+    tempo =   timeit.timeit("insertionSort({})".format(shuffle_random(option)), setup="from __main__ import insertionSort",number=1)
     results_random.append(tempo)
-    tempo =   timeit.timeit("selectionsort({})".format(list(range(option))[::-1]), setup="from __main__ import selectionsort",number=1)
+    tempo =   timeit.timeit("insertionSort({})".format(list(range(option))[::-1]), setup="from __main__ import insertionSort",number=1)
     results_reverse.append(tempo)
 
-plotaImagem(options, results_random, name ='aleatorio.png')
-plotaImagem(options, results_reverse, name='reverso.png')
+plotaImagem(options, results_random, name ='aleatorioInsertion.png')
